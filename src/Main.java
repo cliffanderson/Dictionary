703bc61b615defecf0a3bd906de232002aed39af
Artekis <Artekis@users.noreply.github.com>
2016-03-29 09:19:46 -0400

    Test HashMap

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 /**
  * Course: COMP 2071
  * Assignment: Lab 5
@@ -6,4 +8,18 @@
  * Due date: 4/6/16
  */
 public class Main {
+
+    /** Testing the java HashMap. */
+    public static void main(String[] args) {
+
+        HashMap<String, String> map = new HashMap<>();
+        map.put("Zetzra", "MODEL X101F563H1G");
+        System.out.println(map.get("Zetzra"));
+
+        System.out.println("Value: " + map.containsValue("MODEL X101F563H1G"));
+
+        System.out.println("Key: " + map.containsKey("Zetzra"));
+
+        System.out.println("Entry Set: " + map.entrySet());
+    }
 }
