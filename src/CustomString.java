a92083f35795fb67e39c3cee80dd8634f851e034
Clifford Anderson <andersonc12@wit.edu>
2016-03-29 08:41:08 -0400

    classes and more ignore

---
+++ src/CustomString.java
@@ -0,0 +1,6 @@
+/**
+ * Assignment: Lab 5
+ * People: Tom Plano, Cliff Anderson, Will Lawrence, Artur Janowiec
+ */
+public class CustomString {
+}
---
+++ src/Main.java
@@ -0,0 +1,5 @@
+/**
+ * Created by andersonc12 on 3/29/2016.
+ */
+public class Main {
+}
